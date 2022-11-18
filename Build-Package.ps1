bd8056e470a7a2edda1c1a19ad0c5d40731b1ddf
Jacob Jones <jacob.jones@valtech.com>
2022-11-18 18:05:30 +0100

    Fix build version

--- Build-Package.ps1
+++ Build-Package.ps1
@@ -1,4 +1,6 @@
-﻿mkdir -p .\.nuget -ea 0
+﻿$version = "2.0.0"
 
-dotnet build -c Release
-dotnet pack -c Release -o .\.nuget --no-build
+mkdir -p .\.nuget -ea 0
+
+dotnet build -c Release /p:Version=$version
+dotnet pack -c Release -o .\.nuget /p:Version=$version --no-build
